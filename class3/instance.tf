046fd766de765cba3d228ccb13078f34d26dcfeb
Ahmed Devops <ahmeddevops123@gmail.com>
2020-11-14 13:17:00 +0000

    Instance Added

---
+++ class3/instance.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "Terraform-Created-ubuntu_Insatnce" {
+  ami = "${data.aws_ami.Terraform-Created-Ubuntu_ami.id}"
+
+  #   availability_zone = "${data.aws_availability_zones.available.names[3]}"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Terraform-Created-Ubuntu_EC2"
+  }
+}
+
+resource "aws_instance" "Terraform-Created-Centos_Instance" {
+  ami = "${data.aws_ami.Terraform-Created-Centos_ami.id}"
+
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Terraform-Created-Centos_EC2"
+  }
+}
