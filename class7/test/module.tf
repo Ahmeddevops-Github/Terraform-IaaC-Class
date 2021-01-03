9a18d200eff3143c0efe0f68993b7f175a792a20
Ahmed Aboagye <ahmeddevops123@gmail.com>
2021-01-03 19:32:44 +0000

    RDS DB Module Updated

--- class7/test/module.tf
+++ class7/test/module.tf
@@ -1,15 +1,19 @@
 module "db" {
   source = "../"
   region = "us-east-2"
+
   subnet_ids = [
     "subnet-05b608e6457f5e06a",
-"subnet-0546303e39a5cfe8c",
-"subnet-0f33d7a9c1aa313f6"
+    "subnet-0546303e39a5cfe8c",
+    "subnet-0f33d7a9c1aa313f6",
   ]
+
   security_group_name = "db"
+
   allowed_hosts = [
-    "0.0.0.0/0"
+    "0.0.0.0/0",
   ]
+
   db_name             = "dbname"
   engine              = "mysql"
   engine_version      = "5.7"
@@ -23,12 +27,15 @@ module "db" {
 output "region" {
   value = "${module.db.region}"
 }
+
 output "subnet_list" {
   value = "${module.db.subnet_list}"
 }
+
 output "allowed_hosts" {
   value = "${module.db.allowed_hosts}"
 }
+
 output "DB_NAME" {
   value = "${module.db.DB_NAME}"
 }
